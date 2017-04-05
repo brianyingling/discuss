721905460f84e89354a4669107734c191b4d8cc2
Brian Yingling <brian@jet.com>
2017-04-05 12:39:39 -0400

    ws

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -7,6 +7,7 @@ defmodule Discuss.TopicController do
         render conn, "new.html", changeset: changeset
     end
 
+    
     def create(conn, %{"topic" => topic}) do
         changeset = Topic.changeset(%Topic{}, topic)
         case Repo.insert(changeset) do
